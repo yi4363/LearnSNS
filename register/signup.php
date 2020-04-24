915b45f2e30aba2916de6b8042448ced2e2605dd
yoshi4363 <yoshi4363@gmail.com>
2020-04-24 23:15:29 +0900

    Noエラー時の処理追加
    サインアップ画面完成

--- register/signup.php
+++ register/signup.php
@@ -1,12 +1,19 @@
 <?php
+
+    session_start();
+    // セッション使用宣言
+
     // エラー の種類を保存する配列
     $errors = array();
+
+    date_default_timezone_set("Asia/Tokyo");
+    // タイムゾーン設定
 
     if (!empty($_POST)) {
         $name = $_POST["input_name"];
         $email = $_POST["input_email"];
         $password = $_POST["input_password"];
-        // null, "", 空以外なら通す
+        // null, "", 0以外なら通す
 
         if ($name == "") {
             $errors["name"] = "blank";
@@ -38,6 +45,18 @@
         }else{
             $errors["img_name"] = "blank";
         }
+    }
+
+    // エラーがなかったときの処理
+    if (empty($errors)) {
+        $date_str = date("YmdHis");
+        $submit_file_name = $date_str . $file_name;
+
+        move_uploaded_file($_FILES["input_img_name"]["tmp_name"],"../user_profile_img/" . $submit_file_name);
+        // 画像アップロード
+
+        header("Location: check.php");
+        exit();
     }
 ?>
 <!DOCTYPE html>
