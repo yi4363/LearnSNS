530e60ddc41749ef4f28949ba9b861914d00f05e
yoshi4363 <yoshi4363@gmail.com>
2020-04-25 13:46:20 +0900

    スタイルシート追加 セッション処理追加 登録情報の表示処理追加

--- register/check.php
+++ register/check.php
@@ -1,18 +1,55 @@
 <?php
     session_start();
 
-    echo $_SESSION["register"]["name"] . "<br>";
-    echo $_SESSION["register"]["email"] . "<br>";
-    echo $_SESSION["register"]["password"] . "<br>";
-    echo $_SESSION["register"]["img_name"] . "<br>";
+    $name = $_SESSION["register"]["name"];
+    $email = $_SESSION["register"]["email"];
+    $password = $_SESSION["register"]["password"];
+    $img_name = $_SESSION["register"]["img_name"];
 ?>
 <!DOCTYPE html>
 <html lang="ja">
 <head>
   <meta charset="utf-8">
-  <title></title>
+  <title>LearnSNS</title>
+  <link rel="stylesheet" type="text/css" href="../assets/css/bootstrap.css">
+  <link rel="stylesheet" type="text/css" href="../assets/font-awesome/css/font-awesome.css">
+  <link rel="stylesheet" type="text/css" href="../assets/css/style.css">
 </head>
-<body>
-  <img src="../user_profile_img/<?php echo $_SESSION["register"]["img_name"]; ?>" width="60">
+<body style="margin-top: 60px">
+  <div class="container">
+    <div class="row">
+      <div class="col-xs-8 col-xs-offset thumbnail">
+        <h2 class="text-center content_header">アカウント情報確認</h2>
+        <div class="row">
+          <div class="col-xs-4">
+            <img src="../user_profile_img/<?php echo htmlspecialchars($img_name); ?>" class="img-responsive img-thumbnail">
+          </div>
+          <div class="col-xs-8">
+            <div>
+              <span>ユーザー名</span>
+              <p class="lead"><?php echo htmlspecialchars($name); ?></p>
+              <!-- htmlspecialchars クロスサイトスクリプティング対策-->
+            </div>
+            <div>
+              <span>メールアドレス</span>
+              <p class="lead"><?php echo htmlspecialchars($email); ?></p>
+            </div>
+            <div>
+              <span>パスワード</span>
+              <p class="lead">●●●●●●●●</p>
+            </div>
+            <form action="" method="post">
+              <a href="signup.php?action=rewrite" class="btn-btn-default">&laquo;&nbsp;戻る</a>
+              <input type="hidden" name="action" value="submit">
+              <input type="submit" class="btn btn-primary" value="ユーザー登録">
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <script src="../assets/js/jquery-3.1.1.js"></script>
+  <script src="../assets/js/jquery-migrate-1.4.1.js"></script>
+  <script src="../assets/js/bootstrap.js"></script>
 </body>
 </html>
