fab332c3d22aa2e40a1660382cd201f74ab6f52f
yoshi4363 <yoshi4363@gmail.com>
2020-04-26 14:40:16 +0900

    入力フォーム表示処理

---
+++ signin.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="utf-8">
+  <title>LearnSNS</title>
+  <link rel="stylesheet" type="text/css" href="assets/css/bootstrap.css"> <link rel="stylesheet" type="text/css" href="assets/font-awesome/css/font-awesome.css"> 
+  <link rel="stylesheet" type="text/css" href="assets/css/style.css">
+</head>
+<body style="margin-top: 60PX">
+  <div class="container">
+    <div class="row">
+      <div class="col-xs-8 col-xs-offset-2 thumbnail">
+        <h2 class="text-center content_header">サインイン</h2>
+        <form action="" method="post" enctype="multipart/form-data">
+          <div class="form-group">
+            <label for="email">メールアドレス</label>
+            <input type="email" name="input_email" class="form-control" id="email" placeholder="example@gmail.com">
+          </div>
+          <div class="form-group">
+            <label for="password">パスワード</label>
+            <input type="password" name="input_password" class="form-control" id="password" placeholder="4 ~ 16文字のパスワード">
+          </div>
+          <input type="submit" class="btn btn-info" value="サインイン">
+        </form>
+      </div>
+    </div>
+  </div>
+  <script src="assets/js/jquery-3.1.1.js"></script>
+  <script src="assets/js/jquery-migrate-1.4.1.js"></script>
+  <script src="assets/js/bootstrap.js"></script>
+</body>
+</html>
