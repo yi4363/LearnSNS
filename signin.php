4d0693d26041a95552d2d5e6af273abaab8f2c17
yoshi4363 <yoshi4363@gmail.com>
2020-04-26 15:43:11 +0900

    サインイン画面、バリデーション処理追加

--- signin.php
+++ signin.php
@@ -1,3 +1,38 @@
+<?php
+    // 初期化
+    $errors = array();
+
+    require("dbconnect.php");
+
+    // POST送信された時の処理
+    if (!empty($_POST)) {
+        $email = $_POST["input_email"];
+        $password = $_POST["input_password"];
+
+        // メールアドレスチェック
+        if ($email != "" && $password != "") {
+            $sql = "SELECT * FROM `users` WHERE `email`=?";
+            $data = array($email);
+            $stmt = $dbh->prepare($sql);
+            $stmt->execute($data);
+            $record = $stmt->fetch(PDO::FETCH_ASSOC);
+
+            if ($record == false) {
+                $errors["signin"] = "failed";
+            }
+
+            // パスワードチェック
+            if (password_verify($password,   $record["password"])) {
+                $_SESSION["id"] = $record[  "id"];
+
+                header("Location:   timeline.php");
+                exit();
+            }
+        }else{
+            $errors["signin"] = "blank";
+        }
+    }
+?>
 <!DOCTYPE html>
 <html lang="ja">
 <head>
@@ -11,10 +46,16 @@
     <div class="row">
       <div class="col-xs-8 col-xs-offset-2 thumbnail">
         <h2 class="text-center content_header">サインイン</h2>
-        <form action="" method="post" enctype="multipart/form-data">
+        <form action="signin.php" method="post" enctype="multipart/form-data">
           <div class="form-group">
             <label for="email">メールアドレス</label>
             <input type="email" name="input_email" class="form-control" id="email" placeholder="example@gmail.com">
+            <?php if (isset($errors["signin"]) && $errors["signin"] == "blank"): ?>
+              <p class="text-danger">メールアドレスとパスワードを正しく入力してください</p>
+            <?php endif; ?>
+            <?php if (isset($erros["singin"]) && $errors["signin"] == "failed"): ?>
+              <p class="text-danger">サインインに失敗しました</p>
+            <?php endif; ?>
           </div>
           <div class="form-group">
             <label for="password">パスワード</label>
