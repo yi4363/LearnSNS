c796f6351c88587744e20a454ef17f11f3211f50
yoshi4363 <[yoshi4363_0115@yahoo.co.jp]>
2020-04-27 12:07:54 +0900

    いいね！ボタンの処理追加

--- timeline.php
+++ timeline.php
@@ -128,12 +128,13 @@
               </div>
             </div>
             <div class="row feed_sub">
+              <!-- いいね、Noいいねの判定をDBのフラグにて行う -->
               <div class="col-xs-12">
-                <form method="POST" action="" style="display: inline;">
-                  <input type="hidden" name="feed_id" >
-                    <input type="hidden" name="like" value="like">
-                    <button type="submit" class="btn btn-default btn-xs"><i class="fa fa-thumbs-up" aria-hidden="true"></i>いいね！</button>
-                </form>
+                <?php if ($feed["like_flag"] == 0): ?>
+                  <a href="like.php?feed_id=<?php echo $feed['id']; ?>"><button class="btn btn-default btn-xs"><i class="fa fa-thumbs-up" aria-hidden="true"></i>いいね！</button></a>
+                <?php else: ?>
+                  <a href="unlike.php?feed_id=<?php echo $feed['id']; ?>"><button type="submit" class="btn btn-default btn-xs"><i class="fa fa-thumbs-up" aria-hidden="true"></i>いいね！を取り消す</button>
+                <?php endif; ?>
                 <span class="like_count">いいね数 : 100</span>
                 <span class="comment_count">コメント数 : 9</span>
                   <a href="edit.php?feed_id=<?php echo $feed['id']; ?>" class="btn btn-success btn-xs">編集</a>
